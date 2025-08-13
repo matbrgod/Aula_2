81295e7314d8f8437386cfa247683b6632ef5e33
matbrgod <mcosta.barroso@gmail.com>
2025-08-13 10:38:42 -0300

    Add som da moeda

--- Assets/Script/ItemControl.cs
+++ Assets/Script/ItemControl.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 
 public class ItemControl : MonoBehaviour
 {
-    public int coins;
-    public int health;
-    public int maxHealth = 100;
+    public int coins; // Number of coins collected
+    public int health; // Health of the player
+    public int maxHealth = 100; // Maximum health of the player
+    AudioSource audioSource; // AudioSource to play sound effects
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
@@ -17,9 +20,10 @@ public class ItemControl : MonoBehaviour
         Collider2D collision = Physics2D.OverlapCircle(transform.position, 0.1f, 128);
         if (collision)
         {
-            coins++;
-
-            Destroy( collision.gameObject );   
+            coins++; // Increment coins when colliding with a coin item
+            audioSource.Play(); // Play the coin collection sound
+            Destroy(collision.gameObject); // Destroy the coin item after collecting it
         }
+
     }
 }
